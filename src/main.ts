a22b665015e62502485b1c1a836b7bc5e6d1f0ca
elBanna00 <moaazibrahim3@gmail.com>
2024-05-13 08:56:08 +0300

    style:Add terminal css styling
    - Adds bg color
    - Adds terminal prompt transition
    - Adds glowing effect to all terminal text

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,27 @@ term.pause();
 function ready() {
   term
     .echo(() => {
-      const ascii = render("WTerm");
-      return `${ascii}\nHi,I'm Moaaz\nWelcome to my Website\n`;
+      const ascii = rainbow(render("WTerm"));
+      return `${ascii}[[;#E67E22 ;]\nHi,I'm Moaaz\nWelcome to my Website!\n]`;
     })
     .resume();
+}
+function rainbow(string) {
+  return lolcat
+    .rainbow(function (char, color) {
+      char = $.terminal.escape_brackets(char);
+      return `[[;${hex(color)};]${char}]`;
+    }, string)
+    .join("\n");
+}
+
+function hex(color) {
+  return (
+    "#" +
+    [color.red, color.green, color.blue]
+      .map((n) => {
+        return n.toString(16).padStart(2, "0");
+      })
+      .join("")
+  );
 }
